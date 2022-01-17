5d41718fbd2735ecb42b405d8c3bb6cc06b0c3ff
Anupam Walker <singhsamarth656@gmail.com>
2022-01-17 11:31:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ if os.path.exists(".env"):
     load_dotenv(".env")
 
 # Necessary Vars
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-SESSION = os.getenv("SESSION")
+API_ID = int(os.getenv("API_ID", "7342182"))
+API_HASH = os.getenv("API_HASH", "a80ca7acb06d0cb64a967ba33ef1e536")
+SESSION = os.getenv("SESSION", "BQATRWTiLK-utz3teRqq_WVfJdyGSx2tOFiC7WpnC_utFeY12qBjV8vv-OG75rs6VLgszjAcSrADQKeRNmgpkY3wH9pcLmJso8Vy37OZ1YapY0sUqNQMZaYg5wjwwAsMtPXZkqTHvQiAyDYbsv0zpTnp5Vd3vTTSt6bc9pPi1dEmHd30xHjJEYCiI9v_hlVpmI1_Cxh7knY75HTbkd62lqXmX-BQ6Bzzo2U6nkMM-j6UpvcAv8xM-pWPbIRWWkQquo7VK2Qoo1ICxmPKnovBdxCkXwKiHczmtbHfNR5zHZ4bLwqGvX8ml_MIzmGlR2YOCkamH5xQfioeFK8HQoULaDPqAAAAASyxYRsA")
 HNDLR = os.getenv("HNDLR", "/")
-SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, os.getenv("SUDO_USERS", "1362497646").split()))
 
 
 contact_filter = filters.create(
